49588ecbfa84e43631997368513cb02261e958f6
Praveen Bisht <prvnbist@gmail.com>
2019-07-31 18:15:28 +0530

    Added: Window options

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
-const Header = () => {
-	return <header className="window__header">Header</header>
+const Header = props => {
+	const windowMinimize = () => console.log('Minimize window!')
+	const windowMaximize = () => console.log('Maximize window!')
+	const windowClose = () => console.log('Close window!')
+	return (
+		<header className="window__header">
+			<span className="window__header__title">{props.title}</span>
+			<div className="window__header__actions">
+				<div onClick={() => windowMinimize()}>-</div>
+				<div onClick={() => windowMaximize()}>[]</div>
+				<div onClick={() => windowClose()}>x</div>
+			</div>
+		</header>
+	)
 }
 
 export default Header
