55c4c9e1b60a78683bee218486f2ee020cfa9c29
Praveen Bisht <prvnbist@gmail.com>
2019-07-31 18:27:00 +0530

    Added: Footer actions

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
 const Footer = () => {
-	return <footer className="window__footer">Footer</footer>
+	const SwitchMobileView = () => console.log('Mobile View!')
+	const SwitchTabletView = () => console.log('Tablet View!')
+	const SwitchDesktopView = () => console.log('Desktop View!')
+	return (
+		<footer className="window__footer">
+			<span>Footer</span>
+			<div className="window__responsive__actions">
+				<div onClick={() => SwitchMobileView()}>M</div>
+				<div onClick={() => SwitchTabletView()}>T</div>
+				<div onClick={() => SwitchDesktopView()}>D</div>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
