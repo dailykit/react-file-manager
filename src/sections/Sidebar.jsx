fef40eeef76042f0b97d0b67ad35a0a0fc31d170
Praveen Bisht <prvnbist@gmail.com>
2019-08-02 00:15:58 +0530

    Added: Styles to folder tree

--- src/sections/Sidebar.jsx
+++ src/sections/Sidebar.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Treebeard } from 'react-treebeard'
 
-const data = {
-	name: 'root',
+const folderData = {
+	name: 'Folders',
 	toggled: true,
 	children: [
 		{
@@ -11,13 +11,13 @@ const data = {
 			id: './filesystem/Dishes',
 			children: [
 				{
-					name: 'Dishes',
-					id: './filesystem/Dishes',
+					name: 'Vegetarian',
+					id: './filesystem/Dishes/Vegetarian',
 					children: [
 						{
-							name: 'Dishes',
-							id: './filesystem/Dishes',
-							children: [{}],
+							name: 'Paneer',
+							id: './filesystem/Dishes/Vegetarian/Paneer',
+							children: [],
 							type: 'folder',
 						},
 					],
@@ -30,57 +30,92 @@ const data = {
 			name: 'Ingredients',
 			id: './filesystem/Ingredients',
 
-			children: [{}],
+			children: [],
 			type: 'folder',
 		},
 		{
 			name: 'Menu',
 			id: './filesystem/Menu',
 
-			children: [{}],
+			children: [],
 			type: 'folder',
 		},
 	],
 }
 
-class TreeExample extends Component {
-	constructor(props) {
-		super(props)
-		this.state = { data }
-	}
+const TreeExample = () => {
+	const [data, setData] = React.useState(folderData)
+	const [cursor, setCursor] = React.useState(false)
 
-	onToggle = (node, toggled) => {
-		const { cursor, data } = this.state
+	const onToggle = (node, toggled) => {
 		if (cursor) {
-			this.setState(() => ({ cursor, active: false }))
+			cursor.active = false
 		}
 		node.active = true
 		if (node.children) {
 			node.toggled = toggled
 		}
-		this.setState(() => ({ cursor: node, data: Object.assign({}, data) }))
+		setCursor(node)
+		setData(Object.assign({}, data))
 	}
-	render() {
-		const { data } = this.state
-		return (
-			<Treebeard
-				style={{
-					tree: {
-						base: {
-							backgroundColor: 'white',
-						},
-						node: {
-							activeLink: {
-								background: 'transparent',
-							},
+	const decorators = {
+		Toggle: props => {
+			return (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="17"
+					height="17"
+					viewBox="0 0 24 24"
+					fill={props.toggled ? '#6a91ee' : 'none'}
+					stroke="#6a91ee"
+					strokeWidth="2"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+				>
+					<path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
+				</svg>
+			)
+		},
+		Header: props => {
+			return <span style={{ marginLeft: '8px' }}>{props.node.name}</span>
+		},
+		Container: props => {
+			return (
+				<div
+					onClick={props.onClick}
+					style={{
+						height: '32px',
+						display: 'flex',
+						alignItems: 'center',
+						cursor: 'pointer',
+					}}
+				>
+					<props.decorators.Toggle toggled={props.node.toggled} />
+					<props.decorators.Header {...props} />
+				</div>
+			)
+		},
+	}
+
+	return (
+		<Treebeard
+			style={{
+				tree: {
+					base: {
+						backgroundColor: 'white',
+					},
+					node: {
+						activeLink: {
+							background: 'transparent',
 						},
 					},
-				}}
-				data={data}
-				onToggle={this.onToggle}
-			/>
-		)
-	}
+				},
+			}}
+			data={data}
+			onToggle={onToggle}
+			decorators={decorators}
+		/>
+	)
 }
 
 const Sidebar = props => {
