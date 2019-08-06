218f660bbe63e0fc2dca976f1e834c7e820285ca
Praveen Bisht <prvnbist@gmail.com>
2019-08-06 13:21:16 +0530

    Added: Dynamix breadcrumbs

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -37,28 +37,30 @@ const Navbar = props => {
 				</button>
 			</div>
 			<ul className="window__main__breadcrumbs">
-				{props.breadcrumbs.map((breadcrumb, index) => (
-					<React.Fragment key={index}>
-						<li>{breadcrumb}</li>
-						{index === props.breadcrumbs.length - 1 ? null : (
-							<span>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="18"
-									height="18"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="#000000"
-									strokeWidth="2"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								>
-									<path d="M9 18l6-6-6-6" />
-								</svg>
-							</span>
-						)}
-					</React.Fragment>
-				))}
+				{props.breadcrumbs &&
+					props.breadcrumbs.split('/').map((breadcrumb, index) => (
+						<React.Fragment key={index}>
+							<li>{breadcrumb}</li>
+							{index ===
+							props.breadcrumbs.split('/').length - 1 ? null : (
+								<span>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="18"
+										height="18"
+										viewBox="0 0 24 24"
+										fill="none"
+										stroke="#000000"
+										strokeWidth="2"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+									>
+										<path d="M9 18l6-6-6-6" />
+									</svg>
+								</span>
+							)}
+						</React.Fragment>
+					))}
 			</ul>
 			<div className="window__main__search">
 				<input
