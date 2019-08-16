2bb43f0f6d468b8ff2e2c4a0bf12bd785b227d5c
Praveen Bisht <prvnbist@gmail.com>
2019-08-16 17:49:36 +0530

    Modified: File/Folder row layout

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -5,17 +5,59 @@ import PropTypes from 'prop-types'
 import convertFileSize from '../utils/convertFileSize'
 
 const TableRow = ({ showHidePreview, name, type, size }) => {
-	return (
-		<div
-			className="table__row"
+	const Delete = (
+		<button>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="18"
+				height="18"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="#000000"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<polyline points="3 6 5 6 21 6" />
+				<path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+				<line x1="10" y1="11" x2="10" y2="17" />
+				<line x1="14" y1="11" x2="14" y2="17" />
+			</svg>
+		</button>
+	)
+	const Preview = (
+		<button
 			onClick={() =>
 				showHidePreview({ name, type, size, showHidePreview })
 			}
 		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="18"
+				height="18"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="#000000"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<circle cx="12" cy="12" r="10" />
+				<line x1="12" y1="16" x2="12" y2="12" />
+				<line x1="12" y1="8" x2="12" y2="8" />
+			</svg>
+		</button>
+	)
+	return (
+		<div className="table__row">
 			<div className="item__name">{name}</div>
 			<div className="item__type">{type}</div>
 			<div className="item__size">
 				{size && `${convertFileSize(size)}`}
+			</div>
+			<div className="item__options">
+				{Preview}
+				{Delete}
 			</div>
 		</div>
 	)
