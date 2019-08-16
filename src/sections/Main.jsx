871f087988fb627570bf63f588112fa37e2543cf
Praveen Bisht <prvnbist@gmail.com>
2019-08-16 15:22:10 +0530

    Added: Create folder functionality

--- src/sections/Main.jsx
+++ src/sections/Main.jsx
@@ -3,16 +3,23 @@ import _ from 'lodash'
 import PropTypes from 'prop-types'
 
 import { useQuery } from '@apollo/react-hooks'
+import { useMutation } from '@apollo/react-hooks'
 
 // Components
 import FilePreview from '../components/FilePreview'
 import Card from '../components/Card'
 import TableRow from '../components/TableRow'
+import Modal from '../components/Modal'
 
 // Queries
 import GET_FOLDER from '../queries/getFolder'
+import CREATE_FOLDER from '../queries/createFolder'
 
 const Main = ({ currentFolderPath, view, preview, togglePreview }) => {
+	const [isCreateModalVisible, setCreateModalVisibility] = React.useState(
+		false
+	)
+	const [folderName, setFolderName] = React.useState('')
 	const [previewData, setPreviewData] = React.useState({})
 	const [folderData, setFolderData] = React.useState({
 		name: '',
@@ -31,6 +38,11 @@ const Main = ({ currentFolderPath, view, preview, togglePreview }) => {
 		variables: {
 			path: currentFolderPath,
 		},
+	})
+	const [createFolder, { data: mutationData }] = useMutation(CREATE_FOLDER, {
+		refetchQueries: [
+			{ query: GET_FOLDER, variables: { path: currentFolderPath } },
+		],
 	})
 
 	React.useEffect(() => {
@@ -67,20 +79,108 @@ const Main = ({ currentFolderPath, view, preview, togglePreview }) => {
 	if (queryError) return console.log(queryError) || <div>Error!</div>
 	if (Object.keys(items).length === 0) {
 		return (
-			<div className="empty__state">
+			<div className="window__main empty__state">
+				{isCreateModalVisible && (
+					<Modal>
+						<Modal.Header>Create Folder</Modal.Header>
+						<Modal.Body>
+							<label htmlFor="create__folder__input">
+								Folder Name
+							</label>
+							<input
+								type="text"
+								name="createFolder"
+								id="create__folder__input"
+								value={folderName}
+								placeholder="Enter a folder name"
+								onChange={e => setFolderName(e.target.value)}
+							/>
+						</Modal.Body>
+						<Modal.Footer>
+							<button
+								onClick={() => {
+									createFolder({
+										variables: {
+											path: `${currentFolderPath}/${folderName}`,
+										},
+									})
+									setCreateModalVisibility(
+										!isCreateModalVisible
+									)
+								}}
+							>
+								Create Folder
+							</button>
+							<button
+								onClick={() =>
+									setCreateModalVisibility(
+										!isCreateModalVisible
+									)
+								}
+							>
+								Cancel
+							</button>
+						</Modal.Footer>
+					</Modal>
+				)}
 				<h3>
 					This folder is empty. Start by creating a new folder or a
 					file
 				</h3>
 				<div>
 					<button>Create File</button>
-					<button>Create Folder</button>
+					<button
+						onClick={() =>
+							setCreateModalVisibility(!isCreateModalVisible)
+						}
+					>
+						Create Folder
+					</button>
 				</div>
 			</div>
 		)
 	}
 	return (
 		<main className="window__main">
+			{isCreateModalVisible && (
+				<Modal>
+					<Modal.Header>Create Folder</Modal.Header>
+					<Modal.Body>
+						<label htmlFor="create__folder__input">
+							Folder Name
+						</label>
+						<input
+							type="text"
+							name="createFolder"
+							id="create__folder__input"
+							value={folderName}
+							placeholder="Enter a folder name"
+							onChange={e => setFolderName(e.target.value)}
+						/>
+					</Modal.Body>
+					<Modal.Footer>
+						<button
+							onClick={() => {
+								createFolder({
+									variables: {
+										path: `${currentFolderPath}/${folderName}`,
+									},
+								})
+								setCreateModalVisibility(!isCreateModalVisible)
+							}}
+						>
+							Create Folder
+						</button>
+						<button
+							onClick={() =>
+								setCreateModalVisibility(!isCreateModalVisible)
+							}
+						>
+							Cancel
+						</button>
+					</Modal.Footer>
+				</Modal>
+			)}
 			<div
 				className={`window__main__content ${
 					preview ? 'with__preview' : ''
