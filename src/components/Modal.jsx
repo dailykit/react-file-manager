a8d4f8ff181fff98939e734b5abe88332625e917
Praveen Bisht <prvnbist@gmail.com>
2019-08-23 15:04:19 +0530

    Added: createPortal to render modals at the root

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+
+const modalContainer = document.getElementById('modal__container')
 
 const Header = ({ children }) => {
 	return <div id="modal__card__header">{children}</div>
@@ -11,14 +14,25 @@ const Footer = ({ children }) => {
 }
 
 class Modal extends React.Component {
+	constructor(props) {
+		super(props)
+		this.el = document.createElement('div')
+	}
 	static Header = Header
 	static Body = Body
 	static Footer = Footer
+	componentDidMount() {
+		modalContainer.appendChild(this.el)
+	}
+	componentWillUnmount() {
+		modalContainer.removeChild(this.el)
+	}
 	render() {
-		return (
+		return ReactDOM.createPortal(
 			<div id="modal">
 				<div id="modal__card">{this.props.children}</div>
-			</div>
+			</div>,
+			this.el
 		)
 	}
 }
