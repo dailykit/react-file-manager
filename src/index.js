8bab729348485bb537f4a2827db237b31b0c8ef2
Praveen Bisht <prvnbist@gmail.com>
2019-10-31 12:00:50 +0530

    Fixed: FragmentMatcher throwing error/warning

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { ToastProvider } from 'react-toast-notifications'
 // Apollo Client Imports
 import { ApolloProvider } from '@apollo/react-hooks'
 import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory'
 import { onError } from 'apollo-link-error'
 import { ApolloLink } from 'apollo-link'
 import { createUploadLink } from 'apollo-upload-client'
@@ -18,16 +18,22 @@ import ToastContainer from './components/ToastContainer'
 // Styles
 import './styles/index.scss'
 
-const cache = new InMemoryCache()
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+	introspectionQueryResultData: {
+		__schema: {
+			types: [],
+		},
+	},
+})
+
+const cache = new InMemoryCache({ fragmentMatcher })
 
 const client = new ApolloClient({
 	link: ApolloLink.from([
 		onError(({ graphQLErrors, networkError }) => {
 			if (graphQLErrors)
 				graphQLErrors.map(({ message, locations, path }) =>
-					console.log(
-						`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-					)
+					console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
 				)
 			if (networkError) console.log(`[Network error]: ${networkError}`)
 		}),
@@ -41,11 +47,7 @@ const client = new ApolloClient({
 const Main = () => {
 	return (
 		<ApolloProvider client={client}>
-			<ToastProvider
-				placement="bottom-right"
-				components={{ ToastContainer }}
-				autoDismissTimeout={2000}
-			>
+			<ToastProvider placement="bottom-right" components={{ ToastContainer }} autoDismissTimeout={2000}>
 				<App />
 			</ToastProvider>
 		</ApolloProvider>
