059db96265807cfc8d43bf89943e775ed8ed247b
Praveen Bisht <prvnbist@gmail.com>
2019-12-07 18:13:57 +0530

    refactor(imports)

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Grid from './Grid'
+import FilePreview from './FilePreview'
+import Modal from './Modal'
+import RenderTree from './RenderTree'
+import Table from './Table'
+import ToastContainer from './ToastContainer'
+
+export { Grid, FilePreview, Modal, RenderTree, Table, ToastContainer }
---
+++ src/queries/index.js
@@ -0,0 +1,23 @@
+import CREATE_FILE from './createFile'
+import DELETE_FILE from './deleteFile'
+import RENAME_FILE from './renameFile'
+import CREATE_FOLDER from './createFolder'
+import RENAME_FOLDER from './renameFolder'
+import DELETE_FOLDER from './deleteFolder'
+import GET_FOLDER from './getFolder'
+import GET_NESTED_FOLDER from './getNestedFolders'
+import IMAGE_UPLOAD from './imageUpload'
+import OPEN_FILE from './openFile'
+
+export {
+	CREATE_FILE,
+	DELETE_FILE,
+	RENAME_FILE,
+	CREATE_FOLDER,
+	RENAME_FOLDER,
+	DELETE_FOLDER,
+	GET_FOLDER,
+	GET_NESTED_FOLDER,
+	IMAGE_UPLOAD,
+	OPEN_FILE,
+}
